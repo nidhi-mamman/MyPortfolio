76bfb4c5c0220ce9118015f096ff82346c9a4eee
nidhi <nidhimamman3@gmail.com>
2025-07-20 22:44:10 +0530

    first commit

--- myportfolio/src/Components/Navbar/Navbar.jsx
+++ myportfolio/src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto me-auto mb-2 mb-lg-0">
                         <li class="nav-item me-5 fs-5 fw-bolder">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item me-5 fs-5 fw-bolder">
                             <a class="nav-link" href="#">About</a>
